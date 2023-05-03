d2b8b825caceb31a657ae82bfdf13099521c3d8b
omarb <57on.payment@gmail.com>
2023-05-03 12:02:04 +0300

    route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\farmer;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [farmer::class, 'dashboard'])->name('dashboard');
 
-Route::get('/home', function () {
-    return view('welcome');
-});
+Route::get('/farmers_table', [farmer::class, 'farmers_table'])->name('farmers_table');
+
